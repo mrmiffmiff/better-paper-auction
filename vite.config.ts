dee4edb77e2cea027a3bff5b7435a35657354411
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-03-23 13:05:49 -0700

    Add tailwind and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-	  globals: true,
-	  environment: 'jsdom',
-	  setupFiles: './tests/setup.js',
-  },
+	plugins: [
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './tests/setup.js',
+	},
 })
